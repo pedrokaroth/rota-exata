799ea3613c592f83dc2f35082c95185fe5ab2620
pedrokaroth <pedrokaroth@gmail.com>
2020-11-30 15:32:07 -0300

    first commit

---
+++ source/View.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class View
+{
+    private $engine;
+
+    /**
+     * View constructor.
+     * @param string $path
+     */
+    public function __construct(string $path  = CONV_VIEW_PATH, string $ext = CONF_VIEW_EXT)
+    {
+
+    }
+}
