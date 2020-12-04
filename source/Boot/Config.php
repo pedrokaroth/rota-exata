8611e787fce8d5332137d9d945b0e159d06b58d0
pedrokaroth <pedrokaroth@gmail.com>
2020-12-04 10:57:17 -0300

    Alteração CSS  login

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -10,6 +10,6 @@ define("CONF_VIEW_THEME", "rotaexata");
 /*
  * URL
  */
-define("CONF_URL_BASE", "https://www.rotaexata.droth.com.br");
+define("CONF_URL_BASE", "https://www.rotaexata.projetos.droth.com.br");
 define("CONF_URL_TEST", "https://www.localhost/rota-exata");
 define("CONF_URL_TEST_NAME", "localhost");
